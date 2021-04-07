47ae9211d43a4d3aba862fffca5a819a6a18d1aa
Paul Nevill <paul.nevill@acxiom.com>
2021-04-07 10:53:13 -0500

    updated material to 8

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,14 +5,20 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import {MAT_STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import {throwIfAlreadyLoaded} from './module-import-guard';
 import {HttpClientModule} from '@angular/common/http';
-import {
-  MatButtonModule,
-  MatCardModule, MatChipsModule, MatDividerModule, MatExpansionModule, MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule, MatListModule, MatMenuModule,
-  MatSidenavModule, MatStepperModule,
-  MatToolbarModule, MatTooltipModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const matModules = [
   MatToolbarModule,
